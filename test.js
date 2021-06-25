5bc11113ab94e74dda57a47b2d6c3d881239802c
tonytheonlypony <anthonybone@outlook.com>
2021-06-25 13:18:54 -0700

    simple post and get site

--- test.js
+++ test.js
@@ -1,4 +1,21 @@
 let express = require("express")
 let ourApp = express()
-ourApp.get(a, b)
+ourApp.get('/', function (req, res) {
+    res.send(`
+    <form action="/answer" method="POST">
+        <p>What color is the sky on a clear and sunny day?</p>
+        <input name="skyColor" autocomplete="off">
+        <button>Submit Answer</button>
+    </form>
+    `)
+})
+
+ourApp.post('/answer', function (req, res) {
+
+})
+
+ourApp.get('/answer', function (req, res) {
+    res.send("Are you lost? There is nothing to see here.")
+})
+
 ourApp.listen(3000)
