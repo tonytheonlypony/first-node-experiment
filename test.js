e4828308606a9d3a7e0c13f07dfc67daf62781b8
tonytheonlypony <anthonybone@outlook.com>
2021-06-25 13:25:13 -0700

    simple app done

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 let express = require("express")
 let ourApp = express()
+
+ourApp.use(express.urlencoded({ extended: false }))
+
 ourApp.get('/', function (req, res) {
     res.send(`
     <form action="/answer" method="POST">
@@ -11,7 +14,17 @@ ourApp.get('/', function (req, res) {
 })
 
 ourApp.post('/answer', function (req, res) {
-
+    if (req.body.skyColor.toUpperCase() == "BLUE") {
+        res.send(`
+        <p>Congrats, that is the correct answer!</p>
+        <a href="/">Back to homepage</a>
+        `)
+    } else {
+        res.send(`
+        <p>Sorry, that is incorrect.</p>
+        <a href="/">Back to homepage</a>
+        `)
+    }
 })
 
 ourApp.get('/answer', function (req, res) {
